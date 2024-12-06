e058d841b9b51c45b12be089883eda3b711f336b
NaveenSanjaya <naveensanjayab@gmail.com>
2024-12-06 07:55:16 +0000

    docs: Add image of flower

--- text/QPSK_text_tx_rx.py
+++ text/QPSK_text_tx_rx.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: mpsk_stage6.grc
+# Title: QPSK_text_tx_rxgrc
 # GNU Radio version: 3.10.7.0
 
 from packaging.version import Version as StrictVersion
@@ -31,12 +31,12 @@ import sip
 
 
 
-class mpsk_stage6(gr.top_block, Qt.QWidget):
+class QPSK_text_tx_rx(gr.top_block, Qt.QWidget):
 
-    def __init__(self):
-        gr.top_block.__init__(self, "mpsk_stage6.grc", catch_exceptions=True)
+    def __init__(self, MTU=1500):
+        gr.top_block.__init__(self, "QPSK_text_tx_rxgrc", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("mpsk_stage6.grc")
+        self.setWindowTitle("QPSK_text_tx_rxgrc")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -54,7 +54,7 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "mpsk_stage6")
+        self.settings = Qt.QSettings("GNU Radio", "QPSK_text_tx_rx")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -65,12 +65,20 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
             print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
+        # Parameters
+        ##################################################
+        self.MTU = MTU
+
+        ##################################################
         # Variables
         ##################################################
         self.sps = sps = 4
         self.qpsk = qpsk = digital.constellation_rect([0.707+0.707j, -0.707+0.707j, -0.707-0.707j, 0.707-0.707j], [0, 1, 2, 3],
         4, 2, 2, 1, 1).base()
+        self.polys = polys = [109, 79]
         self.nfilts = nfilts = 32
+        self.k = k = 7
+        self.cc_decoder = cc_decoder = fec.cc_decoder.make(16,7, 2, [79,109], 0, (-1), fec.CC_STREAMING, True)
         self.variable_adaptive_algorithm_0 = variable_adaptive_algorithm_0 = digital.adaptive_algorithm_cma( qpsk, .0001, 4).base()
         self.tx_freq = tx_freq = 600e6
         self.timing_loop_bw = timing_loop_bw = 6.28/100.0
@@ -85,15 +93,18 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.rf_gain = rf_gain = 60
         self.phase_bw = phase_bw = 6.28/100.0
         self.noise_volt = noise_volt = 0.0001
-        self.ldpc_enc = ldpc_enc = fec.ldpc_encoder_make('/home/gnuradio/Documents/DigitalCommDesign/text/ldpc/n_0100_k_0042_gap_02.alist')
-        self.ldpc_dec = ldpc_dec = fec.ldpc_decoder.make('/home/gnuradio/Documents/DigitalCommDesign/text/ldpc/n_0100_k_0042_gap_02.alist', 0.5, 50)
+        self.ldpc_enc = ldpc_enc = fec.ldpc_encoder_make('/home/gnuradio/Documents/DigitalCommDesign/text/ldpc/n_0100_k_0023_gap_10.alist')
+        self.ldpc_dec = ldpc_dec = fec.ldpc_decoder.make('/home/gnuradio/Documents/DigitalCommDesign/text/ldpc/n_0100_k_0023_gap_10.alist', 0.5, 50)
         self.if_gain = if_gain = 40
         self.hdr_format = hdr_format = digital.header_format_default('00011010110011111111110000011101',1, 1)
         self.freq_offset = freq_offset = 0
         self.freq = freq = 2.7e9
         self.excess_bw = excess_bw = 0.5
         self.eq_gain = eq_gain = 0.01
-        self.delay = delay = 50
+        self.enc_cc = enc_cc = fec.cc_encoder_make((MTU*8),k, 2, polys, 0, fec.CC_TERMINATED, True)
+        self.delay = delay = 30
+        self.dec_cc = dec_cc = list(map( (lambda a: fec.cc_decoder.make((MTU*8),k, 2, polys, 0, (-1), fec.CC_TERMINATED, True)),range(0,1)))
+        self.cc_encoder = cc_encoder = fec.cc_encoder_make(16,7, 2, [79,109], 0, fec.CC_STREAMING, True)
         self.arity = arity = 4
 
         ##################################################
@@ -167,7 +178,7 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
             self.controls_grid_layout_0.setRowStretch(r, 1)
         for c in range(1, 2):
             self.controls_grid_layout_0.setColumnStretch(c, 1)
-        self._delay_range = Range(0, 200, 1, 50, 200)
+        self._delay_range = Range(0, 200, 1, 30, 200)
         self._delay_win = RangeWidget(self._delay_range, self.set_delay, "Delay", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._delay_win, 1, 0, 1, 1)
         for r in range(1, 2):
@@ -422,8 +433,8 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self._freq_range = Range(2.7e9, 2.8e9, 100e3, 2.7e9, 200)
         self._freq_win = RangeWidget(self._freq_range, self.set_freq, "'freq'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._freq_win)
-        self.fec_generic_encoder_0 = fec.encoder(ldpc_enc, gr.sizeof_char, gr.sizeof_char)
-        self.fec_generic_decoder_0 = fec.decoder(ldpc_dec, gr.sizeof_char, gr.sizeof_char)
+        self.fec_extended_tagged_encoder_0_1 = fec.extended_tagged_encoder(encoder_obj_list=enc_cc, puncpat='11', lentagname="packet_len", mtu=MTU)
+        self.fec_extended_tagged_decoder_2 = self.fec_extended_tagged_decoder_2 = fec_extended_tagged_decoder_2 = fec.extended_tagged_decoder(decoder_obj_list=dec_cc, ann=None, puncpat='11', integration_period=10000, lentagname="packet_len", mtu=MTU)
         self._eq_gain_range = Range(0.0, 0.1, 0.001, 0.01, 200)
         self._eq_gain_win = RangeWidget(self._eq_gain_range, self.set_eq_gain, "Equalizer: rate", "slider", float, QtCore.Qt.Horizontal)
         self.controls_grid_layout_1.addWidget(self._eq_gain_win, 0, 1, 1, 1)
@@ -433,6 +444,7 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
             self.controls_grid_layout_1.setColumnStretch(c, 1)
         self.digital_protocol_formatter_bb_0 = digital.protocol_formatter_bb(hdr_format, "packet_len")
         self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, timing_loop_bw, rrc_taps, nfilts, (nfilts/2), 1.5, 2)
+        self.digital_map_bb_0_0 = digital.map_bb([-1, 1])
         self.digital_map_bb_0 = digital.map_bb([0,1,2,3])
         self.digital_linear_equalizer_0 = digital.linear_equalizer(15, 2, variable_adaptive_algorithm_0, True, [ ], 'corr_est')
         self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(4, digital.DIFF_DIFFERENTIAL)
@@ -463,12 +475,15 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, "packet_len", 0)
         self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 8, "packet_len")
+        self.blocks_repack_bits_bb_0_0_0 = blocks.repack_bits_bb(1, 8, 'packet_len', False, gr.GR_MSB_FIRST)
+        self.blocks_repack_bits_bb_0_0 = blocks.repack_bits_bb(8, 1, 'packet_len', False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, 8, "", False, gr.GR_MSB_FIRST)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/gnuradio/Documents/DigitalCommDesign/text/tx.txt', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/gnuradio/Documents/DigitalCommDesign/image/flower_tx.jpg', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/gnuradio/Documents/DigitalCommDesign/text/rx.txt', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/gnuradio/Documents/DigitalCommDesign/image/flower__x.jpg', False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, int(delay))
+        self.blocks_char_to_float_1_1 = blocks.char_to_float(1, 1)
         self.blocks_char_to_float_0_0_0 = blocks.char_to_float(1, 1)
         self.blocks_char_to_float_0_0 = blocks.char_to_float(1, 1)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
@@ -480,16 +495,19 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_char_to_float_0_0, 0), (self.qtgui_time_sink_x_0_0, 0))
         self.connect((self.blocks_char_to_float_0_0_0, 0), (self.blocks_delay_0, 0))
+        self.connect((self.blocks_char_to_float_1_1, 0), (self.fec_extended_tagged_decoder_2, 0))
         self.connect((self.blocks_delay_0, 0), (self.qtgui_time_sink_x_0_0, 1))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle2_0_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_unpack_k_bits_bb_0_0, 0))
         self.connect((self.blocks_repack_bits_bb_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.blocks_tagged_stream_mux_0, 1))
-        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_protocol_formatter_bb_0, 0))
+        self.connect((self.blocks_repack_bits_bb_0_0, 0), (self.fec_extended_tagged_encoder_0_1, 0))
+        self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.blocks_tagged_stream_mux_0, 1))
+        self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.digital_protocol_formatter_bb_0, 0))
+        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.blocks_repack_bits_bb_0_0, 0))
         self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_throttle2_1, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.channels_channel_model_0, 0))
         self.connect((self.blocks_throttle2_0_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
-        self.connect((self.blocks_throttle2_1, 0), (self.fec_generic_encoder_0, 0))
+        self.connect((self.blocks_throttle2_1, 0), (self.digital_constellation_modulator_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_char_to_float_0_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0_0, 0), (self.blocks_char_to_float_0_0_0, 0))
@@ -499,25 +517,32 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.digital_diff_decoder_bb_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.fec_generic_decoder_0, 0))
+        self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.digital_map_bb_0_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_constellation_decoder_cb_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.digital_diff_decoder_bb_0, 0), (self.digital_map_bb_0, 0))
         self.connect((self.digital_linear_equalizer_0, 0), (self.digital_costas_loop_cc_0, 0))
         self.connect((self.digital_map_bb_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.digital_map_bb_0_0, 0), (self.blocks_char_to_float_1_1, 0))
         self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_linear_equalizer_0, 0))
         self.connect((self.digital_protocol_formatter_bb_0, 0), (self.blocks_tagged_stream_mux_0, 0))
-        self.connect((self.fec_generic_decoder_0, 0), (self.blocks_repack_bits_bb_0, 0))
-        self.connect((self.fec_generic_encoder_0, 0), (self.digital_constellation_modulator_0, 0))
+        self.connect((self.fec_extended_tagged_decoder_2, 0), (self.blocks_repack_bits_bb_0, 0))
+        self.connect((self.fec_extended_tagged_encoder_0_1, 0), (self.blocks_repack_bits_bb_0_0_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "mpsk_stage6")
+        self.settings = Qt.QSettings("GNU Radio", "QPSK_text_tx_rx")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
+
+    def get_MTU(self):
+        return self.MTU
+
+    def set_MTU(self, MTU):
+        self.MTU = MTU
 
     def get_sps(self):
         return self.sps
@@ -533,12 +558,30 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
         self.qpsk = qpsk
         self.digital_constellation_decoder_cb_0.set_constellation(self.qpsk)
 
+    def get_polys(self):
+        return self.polys
+
+    def set_polys(self, polys):
+        self.polys = polys
+
     def get_nfilts(self):
         return self.nfilts
 
     def set_nfilts(self, nfilts):
         self.nfilts = nfilts
         self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), 0.35, 11*self.sps*self.nfilts))
+
+    def get_k(self):
+        return self.k
+
+    def set_k(self, k):
+        self.k = k
+
+    def get_cc_decoder(self):
+        return self.cc_decoder
+
+    def set_cc_decoder(self, cc_decoder):
+        self.cc_decoder = cc_decoder
 
     def get_variable_adaptive_algorithm_0(self):
         return self.variable_adaptive_algorithm_0
@@ -686,12 +729,30 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
     def set_eq_gain(self, eq_gain):
         self.eq_gain = eq_gain
 
+    def get_enc_cc(self):
+        return self.enc_cc
+
+    def set_enc_cc(self, enc_cc):
+        self.enc_cc = enc_cc
+
     def get_delay(self):
         return self.delay
 
     def set_delay(self, delay):
         self.delay = delay
         self.blocks_delay_0.set_dly(int(int(self.delay)))
+
+    def get_dec_cc(self):
+        return self.dec_cc
+
+    def set_dec_cc(self, dec_cc):
+        self.dec_cc = dec_cc
+
+    def get_cc_encoder(self):
+        return self.cc_encoder
+
+    def set_cc_encoder(self, cc_encoder):
+        self.cc_encoder = cc_encoder
 
     def get_arity(self):
         return self.arity
@@ -701,15 +762,24 @@ class mpsk_stage6(gr.top_block, Qt.QWidget):
 
 
 
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--MTU", dest="MTU", type=intx, default=1500,
+        help="Set MTU [default=%(default)r]")
+    return parser
 
-def main(top_block_cls=mpsk_stage6, options=None):
+
+def main(top_block_cls=QPSK_text_tx_rx, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls()
+    tb = top_block_cls(MTU=options.MTU)
 
     tb.start()
 
