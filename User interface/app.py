42414d31509d567b64df10dca26f93da45ea1c3b
Naveen Sanjaya Basnayake <naveensanjayab@gmail.com>
2024-12-28 07:04:02 +0530

    feat: set default appearance mode and color theme for the UI

--- User interface/app.py
+++ User interface/app.py
@@ -440,5 +440,7 @@ class App(ctk.CTk):
 
 
 if __name__ == "__main__":
+    ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+    ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
     app = App()
     app.mainloop()
